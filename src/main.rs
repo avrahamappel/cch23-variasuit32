e40875bee4d6053f6d8b1feb7edab692e10ea11d
Avraham Appel <aappel248@gmail.com>
2023-11-27 15:58:06 -0500

    Add error struct and extra credit route

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
-use rocket::{get, routes};
+use rocket::{get, response, routes, Responder};
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[derive(Responder)]
+#[response(status = 500)]
+struct Error {
+    message: &'static str,
+}
+
+#[get("/-1/error")]
+fn fake_error() -> Error {
+    Error {
+        message: "This is an error",
+    }
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index]);
+    let rocket = rocket::build().mount("/", routes![index, fake_error]);
 
     Ok(rocket.into())
 }
