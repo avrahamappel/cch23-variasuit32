7783edd568d14ec0c9ee05d788020675b694c6f2
Avraham Appel <aappel248@gmail.com>
2023-12-05 21:30:46 -0500

    Day 4, reindeer strength

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+#![allow(clippy::needless_pass_by_value)]
+#![allow(clippy::no_effect_underscore_binding)]
+
 use std::ffi::OsStr;
 use std::path::PathBuf;
 
-use rocket::{get, routes, Responder};
+use rocket::serde::json::Json;
+use rocket::serde::Deserialize;
+use rocket::{get, post, routes, Responder};
 
 #[get("/")]
 fn index() -> &'static str {
@@ -31,9 +36,29 @@ fn exclusive_cube(nums: PathBuf) -> String {
         .to_string()
 }
 
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Reindeer {
+    // name: &'n str,
+    strength: u32,
+}
+
+#[post("/4/strength", data = "<reindeers>")]
+fn reindeer_cheer(reindeers: Json<Vec<Reindeer>>) -> String {
+    reindeers
+        .iter()
+        .map(|r| r.strength)
+        .sum::<u32>()
+        .to_string()
+}
+
+#[allow(clippy::unused_async)]
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
-    let rocket = rocket::build().mount("/", routes![index, fake_error, exclusive_cube]);
+    let rocket = rocket::build().mount(
+        "/",
+        routes![index, fake_error, exclusive_cube, reindeer_cheer],
+    );
 
     Ok(rocket.into())
 }
