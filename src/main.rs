09a828d32c3b1e337bc4fa8225e3fbf0e9a23f03
Avraham Appel <aappel248@gmail.com>
2023-12-05 22:49:34 -0500

    Day 4, reindeer contest

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@
 #![allow(clippy::no_effect_underscore_binding)]
 
 use std::ffi::OsStr;
+use std::ops::Deref;
 use std::path::PathBuf;
 
 use rocket::serde::json::Json;
-use rocket::serde::Deserialize;
+use rocket::serde::{Deserialize, Serialize};
 use rocket::{get, post, routes, Responder};
 
 #[get("/")]
@@ -38,13 +39,20 @@ fn exclusive_cube(nums: PathBuf) -> String {
 
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
-struct Reindeer {
-    // name: &'n str,
+struct Reindeer<'r> {
+    name: &'r str,
     strength: u32,
+    speed: f64,
+    height: u32,
+    antler_width: u32,
+    snow_magic_power: u32,
+    #[serde(alias = "cAnD13s_3ATeN-yesT3rdAy")]
+    candies_eaten: u32,
+    favorite_food: &'r str,
 }
 
 #[post("/4/strength", data = "<reindeers>")]
-fn reindeer_cheer(reindeers: Json<Vec<Reindeer>>) -> String {
+fn reindeer_cheer(reindeers: Json<Vec<Reindeer<'_>>>) -> String {
     reindeers
         .iter()
         .map(|r| r.strength)
@@ -52,12 +60,63 @@ fn reindeer_cheer(reindeers: Json<Vec<Reindeer>>) -> String {
         .to_string()
 }
 
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Winners {
+    fastest: String,
+    tallest: String,
+    magician: String,
+    consumer: String,
+}
+
+impl From<&[Reindeer<'_>]> for Winners {
+    fn from(reindeers: &[Reindeer<'_>]) -> Self {
+        let fastest = reindeers
+            .iter()
+            .max_by(|a, b| a.speed.total_cmp(&b.speed))
+            .unwrap();
+        let tallest = reindeers.iter().max_by_key(|r| r.height).unwrap();
+        let magician = reindeers.iter().max_by_key(|r| r.snow_magic_power).unwrap();
+        let consumer = reindeers.iter().max_by_key(|r| r.candies_eaten).unwrap();
+
+        Self {
+            fastest: format!(
+                "Speeding past the finish line with a strength of {} is {}",
+                fastest.strength, fastest.name
+            ),
+            tallest: format!(
+                "{} is standing tall with his {} cm wide antlers",
+                tallest.name, tallest.antler_width
+            ),
+            magician: format!(
+                "{} could blast you away with a snow magic power of {}",
+                magician.name, magician.snow_magic_power
+            ),
+            consumer: format!(
+                "{} ate lots of candies, but also some {}",
+                consumer.name, consumer.favorite_food
+            ),
+        }
+    }
+}
+
+#[post("/4/contest", data = "<reindeers>")]
+fn reindeer_candy(reindeers: Json<Vec<Reindeer<'_>>>) -> Json<Winners> {
+    Json(Winners::from(reindeers.deref().as_slice()))
+}
+
 #[allow(clippy::unused_async)]
 #[shuttle_runtime::main]
 async fn main() -> shuttle_rocket::ShuttleRocket {
     let rocket = rocket::build().mount(
         "/",
-        routes![index, fake_error, exclusive_cube, reindeer_cheer],
+        routes![
+            index,
+            fake_error,
+            exclusive_cube,
+            reindeer_cheer,
+            reindeer_candy
+        ],
     );
 
     Ok(rocket.into())
