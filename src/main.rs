2bdadb29654d7ec085f801651c2dd20c6922ff87
Avraham Appel <aappel248@gmail.com>
2023-12-08 12:16:47 -0500

    day 4 test

--- src/main.rs
+++ src/main.rs
@@ -77,6 +77,43 @@ fn exclusive_cube_test() {
 
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
+struct SimpleReindeer {
+    strength: u32,
+}
+
+#[post("/4/strength", data = "<reindeers>")]
+fn reindeer_cheer(reindeers: Json<Vec<SimpleReindeer>>) -> String {
+    reindeers
+        .iter()
+        .map(|r| r.strength)
+        .sum::<u32>()
+        .to_string()
+}
+
+#[cfg(test)]
+#[test]
+fn reindeer_cheer_test() {
+    use rocket::http::ContentType;
+
+    let client = test_client!();
+    let response = client
+        .post("/4/strength")
+        .header(ContentType::JSON)
+        .body(
+            r#"[
+    { "name": "Dasher", "strength": 5 },
+    { "name": "Dancer", "strength": 6 },
+    { "name": "Prancer", "strength": 4 },
+    { "name": "Vixen", "strength": 7 }
+  ]"#,
+        )
+        .dispatch();
+
+    assert_eq!("22", response.into_string().unwrap());
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
 struct Reindeer<'r> {
     name: &'r str,
     strength: u32,
@@ -87,15 +124,6 @@ struct Reindeer<'r> {
     #[serde(rename = "cAnD13s_3ATeN-yesT3rdAy")]
     candies_eaten: u32,
     favorite_food: &'r str,
-}
-
-#[post("/4/strength", data = "<reindeers>")]
-fn reindeer_cheer(reindeers: Json<Vec<Reindeer<'_>>>) -> String {
-    reindeers
-        .iter()
-        .map(|r| r.strength)
-        .sum::<u32>()
-        .to_string()
 }
 
 #[derive(Serialize)]
@@ -143,6 +171,46 @@ fn reindeer_candy(reindeers: Json<Vec<Reindeer<'_>>>) -> Json<Winners> {
     Json(Winners::from(reindeers.deref().as_slice()))
 }
 
+#[cfg(test)]
+#[test]
+fn reindeer_candy_test() {
+    use rocket::http::ContentType;
+
+    let client = test_client!();
+    let response = client
+        .post("/4/contest")
+        .header(ContentType::JSON)
+        .body(
+            r#"[
+    {
+      "name": "Dasher",
+      "strength": 5,
+      "speed": 50.4,
+      "height": 80,
+      "antler_width": 36,
+      "snow_magic_power": 9001,
+      "favorite_food": "hay",
+      "cAnD13s_3ATeN-yesT3rdAy": 2
+    },
+    {
+      "name": "Dancer",
+      "strength": 6,
+      "speed": 48.2,
+      "height": 65,
+      "antler_width": 37,
+      "snow_magic_power": 4004,
+      "favorite_food": "grass",
+      "cAnD13s_3ATeN-yesT3rdAy": 5
+    }
+  ]"#,
+        )
+        .dispatch();
+
+    assert_eq!(
+        r#"{"fastest":"Speeding past the finish line with a strength of 5 is Dasher","tallest":"Dasher is standing tall with his 36 cm wide antlers","magician":"Dasher could blast you away with a snow magic power of 9001","consumer":"Dancer ate lots of candies, but also some grass"}"#,
+        response.into_string().unwrap()
+    );
+}
 #[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
 struct ElfCount {
