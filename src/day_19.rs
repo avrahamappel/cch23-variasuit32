429c23a2e4eb43be4d26b97fc2f425223d2a014e
Avraham Appel <aappel248@gmail.com>
2023-12-28 22:58:57 -0500

    wip 2
    Using channels to send messages between users
    Not compiling yet due to borrow checker
    
    - Removed rooms in favor of a direct users vec with room ids
    - Changed user id to &str, and users vec to vec<&user>

--- src/day_19.rs
+++ src/day_19.rs
@@ -1,5 +1,7 @@
 use std::sync::{RwLock, RwLockReadGuard, RwLockWriteGuard};
 
+use rocket::futures::channel::mpsc::{self, Receiver, Sender};
+use rocket::futures::prelude::*;
 use rocket::serde::json::serde_json;
 use rocket::serde::{Deserialize, Serialize};
 use rocket::{get, post, routes, Route};
@@ -26,24 +28,29 @@ fn ws_ping(ws: ws::WebSocket) -> ws::Stream![] {
     }
 }
 
-struct Room {
-    id: u32,
-    users: Vec<String>,
+struct User<'u> {
+    id: &'u str,
+    room_id: u32,
+    channel: (Sender<RoomMessage<'u>>, Receiver<RoomMessage<'u>>),
 }
 
-impl Room {
-    fn new(id: u32) -> Self {
-        Self { id, users: vec![] }
+impl<'u> User<'u> {
+    fn new(id: &'u str, room_id: u32) -> Self {
+        Self {
+            id,
+            room_id,
+            channel: mpsc::channel(MSG_CHAR_LIMIT),
+        }
     }
 }
 
 #[derive(Default)]
-struct State {
+struct State<'u> {
     views: u32,
-    rooms: Vec<Room>,
+    users: Vec<&'u User<'u>>,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
 struct UserMessage {
     message: String,
@@ -57,33 +64,19 @@ impl TryFrom<&str> for UserMessage {
     }
 }
 
-impl From<UserMessage> for Message {
-    fn from(value: UserMessage) -> Self {
-        Message::text(
-            serde_json::to_string(&value).expect("JSON serialization failed for some reason"),
-        )
-    }
-}
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize)]
 #[serde(crate = "rocket::serde")]
-struct RoomMessage {
-    user: String,
+struct RoomMessage<'u> {
+    user: &'u str,
     message: String,
 }
 
-impl TryFrom<&str> for RoomMessage {
-    type Error = Error;
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        let msg = serde_json::from_str(value)?;
-        Ok(msg)
-    }
-}
 
 const MSG_CHAR_LIMIT: usize = 128;
 
-impl RoomMessage {
-    fn new(user: String, message: String) -> Option<Self> {
+impl<'u> RoomMessage<'u> {
+    fn new(user: &'u str, message: String) -> Option<Self> {
         if message.chars().count() > MSG_CHAR_LIMIT {
             None
         } else {
@@ -92,7 +85,7 @@ impl RoomMessage {
     }
 }
 
-impl From<RoomMessage> for Message {
+impl<'u> From<RoomMessage<'u>> for Message {
     fn from(value: RoomMessage) -> Self {
         Message::text(
             serde_json::to_string(&value).expect("JSON serialization failed for some reason"),
@@ -100,32 +93,9 @@ impl From<RoomMessage> for Message {
     }
 }
 
-impl State {
-    fn get_room_mut<'a>(&'a mut self, id: u32) -> &'a mut Room {
-        match self.rooms.iter_mut().find(|r| r.id == id) {
-            Some(room) => room,
-            None => {
-                let mut room = Room::new(id);
-                let borrow = &mut room;
-                self.rooms.push(room);
-                borrow
-            }
-        }
-    }
-
-    fn broadcast_msg_to_user(&mut self, msg: &RoomMessage, user_id: &String) {
-        if self
-            .rooms
-            .iter()
-            .any(|r| r.users.contains(user_id) && r.users.contains(&msg.user))
-        {
-            self.views += 1;
-        }
-    }
-}
 
 pub struct ChatState {
-    state: RwLock<State>,
+    state: RwLock<State<'static>>,
 }
 
 impl ChatState {
@@ -141,7 +111,7 @@ impl ChatState {
     }
 
     fn write(&self) -> Result<RwLockWriteGuard<'_, State>, Error> {
-        let mut state = self.state.write()?;
+        let state = self.state.write()?;
         Ok(state)
     }
 }
@@ -164,39 +134,56 @@ fn ws_room<'r>(
     chat_state: &'r rocket::State<ChatState>,
     ws: ws::WebSocket,
     room_id: u32,
-    user_id: String,
-) -> ws::Stream!['r] {
-    ws::Stream! { ws =>
-        // Connect user to room
-        if let Ok(mut state) = chat_state.write() {
-            let room = state.get_room_mut(room_id);
-            room.users.push(user_id.clone());
-        }
+    user_id: &'r str,
+) -> ws::Channel<'r> {
+    ws.channel(move |mut stream| {
+        Box::pin(async {
+            // Connect user to room
+            let mut state = chat_state
+                .write()
+                .expect("couldn't get write lock on state to add user");
+            let user = User::new(user_id, room_id);
+            state.users.push(&user);
 
-        for await msg in ws {
-            if let Message::Text(text) = msg? {
-                // Sending a message to the room
-                if let Ok(user_msg) = UserMessage::try_from(text.as_str()) {
-                    if let Some(room_msg) = RoomMessage::new(user_id.clone(), user_msg.message) {
-                        yield room_msg.into();
+            let res = future::join(
+                Box::pin(async {
+                    while let Some(msg) = stream.next().await {
+                        if let Message::Text(text) = msg? {
+                            // Sending a message to the room
+                            if let Ok(user_msg) = UserMessage::try_from(text.as_str()) {
+                                if let Some(room_msg) = RoomMessage::new(user_id, user_msg.message)
+                                {
+                                    if let Ok(state) = chat_state.read() {
+                                        for user in state.users {
+                                            user.channel.0.send(room_msg);
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
-                }
-
-                // Receiving a message from the room
-                if let Ok(room_msg) = RoomMessage::try_from(text.as_str()) {
-                    if let Ok(mut state) = chat_state.write() {
-                        state.broadcast_msg_to_user(&room_msg, &user_id);
+                    Ok(())
+                }),
+                Box::pin(async {
+                    // Receiving a message from the room
+                    while let Some(room_msg) = user.channel.1.next().await {
+                        stream.send(room_msg.into()).await;
                     }
-                }
-            }
-        }
+                    Ok(())
+                }),
+            )
+            .await;
 
-        if let Ok(mut state) = chat_state.write() {
-            for room in &mut state.rooms {
-                room.users.retain(|u|u != &user_id);
+            if let Ok(mut state) = chat_state.write() {
+                state.users.retain(|u| u.id != user_id);
             }
-        }
-    }
+
+            match res {
+                (Err(e), _) | (_, Err(e)) => Err(e),
+                _ => Ok(()),
+            }
+        })
+    })
 }
 
 pub fn routes() -> Vec<Route> {
