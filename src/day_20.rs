adc76ced3ad33a3fe3b274216b827d499b6f07b5
Avraham Appel <aappel248@gmail.com>
2024-01-01 18:28:54 -0500

    day 20 part 1 task 2

--- src/day_20.rs
+++ src/day_20.rs
@@ -15,6 +15,19 @@ fn tar_file_count(tarball: TempFile) -> Result<String, Error> {
     Ok(count.to_string())
 }
 
+#[post("/archive_files_size", data = "<tarball>")]
+fn tar_files_size(tarball: TempFile) -> Result<String, Error> {
+    let mut arc = Archive::new(File::open(tarball.path().expect("temp file had no path"))?);
+
+    let sum = arc
+        .entries()?
+        .filter_map(Result::ok)
+        .map(|e| e.size())
+        .sum::<u64>();
+
+    Ok(sum.to_string())
+}
+
 pub fn routes() -> Vec<Route> {
-    routes![tar_file_count]
+    routes![tar_file_count, tar_files_size]
 }
