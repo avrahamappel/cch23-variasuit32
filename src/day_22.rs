fb0fef95f35b70de9e415818238b20bb53a803a7
Avraham Appel <aappel248@gmail.com>
2024-11-04 23:04:42 -0500

    Increase data limit for day 22-1 to 10mb

--- src/day_22.rs
+++ src/day_22.rs
@@ -1,16 +1,23 @@
+use rocket::data::{Data, ToByteUnit};
 use rocket::{post, routes, Route};
 
+use crate::common::Error;
+
 #[post("/integers", data = "<int_strs>")]
-fn integers(int_strs: &str) -> String {
-    // XOR all strings to find the unique one
-    // This only works because there is exactly one unique string
+async fn integers(int_strs: Data<'_>) -> Result<String, Error> {
     let uniq_int = int_strs
+        // One of the tests has a very large input
+        .open(10.megabytes())
+        .into_string()
+        .await?
         .split_whitespace()
         .map(str::parse::<usize>)
         .filter_map(Result::ok)
+        // XOR all strings to find the unique one
+        // This only works because there is exactly one unique string
         .fold(0, |acc, int| acc ^ int);
 
-    "🎁".repeat(uniq_int)
+    Ok("🎁".repeat(uniq_int))
 }
 
 pub fn routes() -> Vec<Route> {
